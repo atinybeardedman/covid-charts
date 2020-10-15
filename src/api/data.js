c4fe37e2870e7d20fb6754e9bbf9e297d1e9314b
atinybeardedman <sedickinson9123@gmail.com>
2020-10-15 19:27:13 -0400

    lint

--- src/api/data.js
+++ src/api/data.js
@@ -1,25 +1,23 @@
-import httpClient from './httpClient';
-import {counties} from '../constants/constants';
-function getWhereString(){
-    let str = '(';
-    const last = counties.pop();
-    for(const county of counties){
-        str += `county = "${county}" OR `
-    }
-    str += `county = "${last}"`;
-    str += `) AND (test_date >= "2020-07-01")`;
-    return str;
+import httpClient from "./httpClient";
+import { counties } from "../constants/constants";
+function getWhereString() {
+  let str = "(";
+  const last = counties.pop();
+  for (const county of counties) {
+    str += `county = "${county}" OR `;
+  }
+  str += `county = "${last}"`;
+  str += `) AND (test_date >= "2020-07-01")`;
+  return str;
 }
 
-const getAllData = () => 
-    httpClient.get(process.env.VUE_APP_BASE_URL,{
-        params: {
-            $where: getWhereString(),
-            $select: "test_date,county,new_positives,total_number_of_tests"
-        }
+const getAllData = () =>
+  httpClient
+    .get(process.env.VUE_APP_BASE_URL, {
+      params: {
+        $where: getWhereString(),
+        $select: "test_date,county,new_positives,total_number_of_tests",
+      },
     })
 
-
-export {
-    getAllData
-}
+export { getAllData };
