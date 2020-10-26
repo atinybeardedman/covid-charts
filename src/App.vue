be3c41a574b6c3a6282f9457e7205fac9c015af9
Sean Dickinson <contact@seandickinson.dev>
2020-10-26 15:42:19 -0400

    feat: added icons and manifest

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="black" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Mid-Hudson Region COVID Trends</v-toolbar-title>
       <v-spacer></v-spacer>
