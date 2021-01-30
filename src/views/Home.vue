05bab72ad8d97e103f8b059d4d682283a0ab08d7
Sean Dickinson <contact@seandickinson.dev>
2021-01-30 12:25:18 -0500

    fix: mobile font adjustment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-row>
       <v-col :md="6">
-        <h2 class="text-h2 pa-2">NYS Regional Covid Dashboard</h2>
+        <h2 class="text-lg-h2 text-h4 pa-2">NYS Regional Covid Dashboard</h2>
         <p class="pa-2">This dashboard provides New York State COVID-19 data and collates it by region and county to more easily track how COVID is spreading in the state. Choose a county of interest either on the interactive map below or the menu on the side.</p>
       </v-col>
     </v-row>
